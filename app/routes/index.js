ca6e41efd4f57575984709a14817ccb5e4d96c09
Rohit Patil <rohitpatil07122001@gmail.com>
2021-10-09 19:10:29 +0530

    Rename cake routes
    Signed-off-by: Rohit Patil <rohitpatil07122001@gmail.com>

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,7 +2,7 @@ import { Router } from 'express';
 import authRoutes from './auth.routes.js';
 import cartRoutes from './cart.routes.js';
 import searchRoutes from './search.routes.js';
-import cakeRoutes from './cakeroutes.js';
+import cakeRoutes from './cake.routes.js';
 
 const router = Router();
 
